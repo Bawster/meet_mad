ab744840c1c9a63ef86add2e0e0d7df8fa48eb9b
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-04-06 08:54:00 +0200

    ahora

--- basededatos/bases.sql
+++
@@ -1,7 +0,0 @@
-drop DATABASE IF EXISTS entornos;
-CREATE DATABASE entornos;
-use entornos
-CREATE TABLE Persona(Nick VARCHAR(50), Nombre VARCHAR(59), Apellido VARCHAR(59), Email VARCHAR(50),
-Contraeña VARCHAR(50), Edad INT, PRIMARY KEY(Nick));
-CREATE TABLE Empresa(Nombre VARCHAR(59), VerificacionEmpresa VARCHAR(59), Ubicacion VARCHAR(50),
-Horario VARCHAR(50), Descripcion VARCHAR(70));
